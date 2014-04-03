0ff05448d9ae0efb1dc792782263e050a392ecd5
sambeaven <sambeaven@gmail.com>
2014-04-03 13:06:14 +0100

    Refactored messages logic into the Placeplayer method. Note: messages stack now loses state. Need to refactor into a class that can maintain state in a property.

--- Rougelike/Program.cs
+++ Rougelike/Program.cs
@@ -13,14 +13,14 @@ namespace Rougelike
         static void Main(string[] args)
         {
             //draw dungeon
-            Stack<Tuple<ConsoleColor, string>> messages = new Stack<Tuple<ConsoleColor, string>>();
             var map = DrawMap();
 
             var hero = new GameLogic.RLAgent(locationX: 5, locationY: 10, displayChar: '@', hitPoints: 200, strength: 50, dexterity: 50);
 
             int? playerDestinationX = null, playerDestinationY = null;
             //place player
-            PlacePlayer(map, hero, hero.locationX, hero.locationY, messages);
+            Console.SetCursorPosition(hero.locationX, hero.locationY);
+            Console.Write(hero.DisplayChar);
 
 
             ConsoleKeyInfo keyInfo;
@@ -32,34 +32,28 @@ namespace Rougelike
 
                 keyInfo = Console.ReadKey(true);
 
-                //TODO: re-architect this: move message logic down into the TakeTurn method. Figure out direction after running through PlacePlayer()
-                //(maybe we should re-architect all of this into a class?)
+                
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.UpArrow:
                         playerDestinationY = hero.locationY - 1;
-                        messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step north"));
                         break;
                     case ConsoleKey.DownArrow:
                         playerDestinationY = hero.locationY + 1;
-                        messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step south"));
                         break;
                     case ConsoleKey.LeftArrow:
                         playerDestinationX = hero.locationX - 1;
-                        messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step west"));
                         break;
                     case ConsoleKey.RightArrow:
                         playerDestinationX = hero.locationX + 1;
-                        messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step east"));
                         break;
                     case ConsoleKey.Spacebar:
-                        messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You wait"));
                         break;
                     default:
                         break;
                 }
 
-                TakeTurn(map, hero, playerDestinationX, playerDestinationY, messages);
+                TakeTurn(map, hero, playerDestinationX, playerDestinationY);
 
 
             } while (keyInfo.Key != ConsoleKey.Escape);
@@ -68,8 +62,12 @@ namespace Rougelike
             //Console.ReadLine();
         }
 
-        private static void TakeTurn(GameLogic.RLMap map, GameLogic.RLAgent hero, int? x, int? y, Stack<Tuple<ConsoleColor, string>> messages)
+        //TODO: re-architect TakeTurn and PlacePlayer into a class (DisplayInfo? Game?) - this will allow the messages stack to keep state.
+        private static void TakeTurn(GameLogic.RLMap map, GameLogic.RLAgent hero, int? x, int? y)
         {
+            Stack<Tuple<ConsoleColor, string>> messages = new Stack<Tuple<ConsoleColor, string>>();
+
+
             DrawMap();
             messages = PlacePlayer(map, hero, x, y, messages);
 
@@ -80,8 +78,6 @@ namespace Rougelike
             int i = 0;
             foreach (var message in messages)
             {
-
-
                 Console.SetCursorPosition(0, 20 + i);
 
                 Console.ForegroundColor = message.Item1;
@@ -98,23 +94,42 @@ namespace Rougelike
         {
             if ((x.HasValue && y.HasValue) && map.isLocationPassable(x.Value, y.Value))
             {
+                if (y < hero.locationY)
+                {
+                    //y - 1 == north
+                    messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step north"));
+                }
+                else if (y > hero.locationY)
+                {
+                    //y + 1 == south
+                    messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step south"));
+                }
+                else if (x < hero.locationX)
+                {
+                    //x - 1 == west
+                    messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step west"));
+                }
+                else if (x > hero.locationX)
+                {
+                    //x + 1 == east
+                    messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Green, "You step east"));
+                }
+                else
+                {
+                    //no change == wait
+                    messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Cyan, "You wait"));
+                }
 
                 hero.locationY = y.Value;
                 hero.locationX = x.Value;
-
-
-                Console.SetCursorPosition(x.Value, y.Value);
-                Console.Write(hero.DisplayChar);
-
-                
             }
             else
             {
-                Console.SetCursorPosition(hero.locationX, hero.locationY);
-                Console.Write(hero.DisplayChar);
+
                 messages.Push(new Tuple<ConsoleColor, string>(ConsoleColor.Red, "Ouch! You walk into a wall."));
             }
-
+            Console.SetCursorPosition(hero.locationX, hero.locationY);
+            Console.Write(hero.DisplayChar);
             return messages;
         }
 
