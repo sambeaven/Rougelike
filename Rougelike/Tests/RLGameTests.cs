777adcf49fc642152fcd4ef6c15a8b0967e19fe2
sambeaven <darkhawke@gmail.com>
2014-04-19 17:01:41 +0100

    Added tests for player movement

--- Rougelike/Tests/RLGameTests.cs
+++ Rougelike/Tests/RLGameTests.cs
@@ -23,36 +23,24 @@ namespace Rougelike.Tests
         [Test]
         public void SetUpCreatesAgentsAndMap()
         {
-            var RLGame = new GameLogic.RLGame();
-            RLGame.SetUp();
+            var levelGenerator = GetMockLevelGenerator();
 
-            Assert.IsNotNull(RLGame.map);
-            Assert.IsTrue(RLGame.agents.Count() > 0);
-        }
+            var game = new GameLogic.RLGame(new GameLogic.RLRenderer(), levelGenerator.Object);
 
-        [Test]
-        public void TakeTurnDoesNotMoveHeroWhenSpaceBarIsPassed()
-        {
-            var game = new GameLogic.RLGame();
             game.SetUp();
 
-            var hero = game.agents.Where(a => a.GetType() == typeof(GameLogic.RLHero)).FirstOrDefault();
 
-            int initialX = hero.locationX;
-            int initialY = hero.locationY;
-
-            Assert.IsNotNull(hero);
-
-            game.TakeTurn(new ConsoleKeyInfo(keyChar:' ', key:ConsoleKey.Spacebar, shift:false, alt:false, control:false));
-
-            hero = game.agents.Where(a => a.GetType() == typeof(GameLogic.RLHero)).FirstOrDefault();
-
-            Assert.AreEqual(initialX, hero.locationX);
-            Assert.AreEqual(initialY, hero.locationY);
+            Assert.IsNotNull(game.map);
+            Assert.IsTrue(game.agents.Count() > 0);
         }
 
-        [Test]
-        public void MoqTest()
+
+        [TestCase(1, 0, ConsoleKey.UpArrow)]
+        [TestCase(1, 2, ConsoleKey.DownArrow)]
+        [TestCase(0, 1, ConsoleKey.LeftArrow)]
+        [TestCase(2, 1, ConsoleKey.RightArrow)]
+        [TestCase(1, 1, ConsoleKey.Spacebar)]
+        public void TakeTurnHandlesMove(int expectedX, int expectedY, ConsoleKey keyToPass)
         {
             var levelGenerator = GetMockLevelGenerator();
 
@@ -60,7 +48,30 @@ namespace Rougelike.Tests
 
             game.SetUp();
 
+            var ConsoleKeyInfo = new ConsoleKeyInfo(keyChar: ' ', key: keyToPass, shift: false, alt: false, control: false);
+
+            game.TakeTurn(ConsoleKeyInfo);
+
+            var hero = GetHero(game);
+
+            Assert.AreEqual(expectedX, hero.locationX);
+            Assert.AreEqual(expectedY, hero.locationY);
+
+            if (keyToPass == ConsoleKey.Spacebar)
+            {
+                Assert.IsTrue(game.messages.First().Item2 == "You wait");
+
+            }
+
         }
+
+        private static GameLogic.RLAgent GetHero(GameLogic.RLGame game)
+        {
+            return game.agents.Where(a => a.GetType() == typeof(GameLogic.RLHero))
+                            .FirstOrDefault();
+        }
+
+
 
         private static Mock<GameLogic.Interfaces.IRLLevelGenerator> GetMockLevelGenerator()
         {
@@ -68,15 +79,15 @@ namespace Rougelike.Tests
 
             levelGenerator.Setup(l => l.GenerateMap())
                 .Returns(new GameLogic.RLMap(GameLogic.RLMap.MapType.emptyMap, mapHeight: 3, mapWidth: 3){
-                    new GameLogic.RLCell() { X = 0, Y = 0},
-                    new GameLogic.RLCell() { X = 1, Y = 0},
-                    new GameLogic.RLCell() { X = 2, Y = 0},
-                    new GameLogic.RLCell() { X = 0, Y = 1},
-                    new GameLogic.RLCell() { X = 1, Y = 1},
-                    new GameLogic.RLCell() { X = 2, Y = 1},
-                    new GameLogic.RLCell() { X = 0, Y = 2},
-                    new GameLogic.RLCell() { X = 1, Y = 2},
-                    new GameLogic.RLCell() { X = 2, Y = 2}
+                    new GameLogic.RLCell() { X = 0, Y = 0, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 1, Y = 0, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 2, Y = 0, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 0, Y = 1, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 1, Y = 1, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 2, Y = 1, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 0, Y = 2, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 1, Y = 2, Passable=true, Unoccupied=true},
+                    new GameLogic.RLCell() { X = 2, Y = 2, Passable=true, Unoccupied=true}
                 });
 
             levelGenerator.Setup(l => l.GenerateAgents(GameLogic.RLLevelGenerator.agentGeneratorBehaviour.IncludeHero))
